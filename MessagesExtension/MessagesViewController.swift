6e26ad37277e0ad2562d811cb82f7fa5c85fb80d
David Lee <designforhuman@gmail.com>
2016-09-05 14:46:28 +0900

    rock paper scissors stickers added as MSSticker

--- MessagesExtension/MessagesViewController.swift
+++ MessagesExtension/MessagesViewController.swift
@@ -14,6 +14,9 @@ class MessagesViewController: MSMessagesAppViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        print("loaded")
+        
     }
     
     override func didReceiveMemoryWarning() {
